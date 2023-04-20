0c7fb6ac67de0e3ac9860282be1ec1fac43e9b4f
FelipeAlvesLeao <82793324+FelipeAlvesLeao@users.noreply.github.com>
2023-04-19 23:29:32 -0300

    Projeto finalizado

---
+++ README.md
@@ -0,0 +1,2 @@
+Este é um projeto feito seguindo o tutorial localizado em Este é um projeto feito seguindo o tutorial localizado em https://youtu.be/LXRU-Z36GEU, no qual é criado um sistema para mapear vagas de um estacionamento em um condomínio e salva-las no PQSL.
+
---
+++ src/main/java/com/example/serverdemo/repositories/ParkingSpotRepository.java
@@ -0,0 +1,14 @@
+package com.example.serverdemo.repositories;
+
+import com.example.serverdemo.models.ParkingSpotModel;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+    boolean existsByLicensePlateCar(String licensePlateCar);
+    boolean existsByParkingSpotNumber(String parkingSpotNumber);
+    boolean existsByApartmentAndBlock(String apartment, String block);
+
+}
